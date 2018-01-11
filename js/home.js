16fa53c1f64249e28acc38ef561e7c5f503f011e
Jilada Eccleston <jilada.eccleston@gmail.com>
2018-01-12 10:15:04 +1300

    Added icons at the bottom with semi working links. Need to add the instagram and linkedin links

--- js/home.js
+++ js/home.js
@@ -1,16 +1,34 @@
+// This function will make the randomly generated name
 const makeName = () => {
   const choices = ["a", "o", "i", "ava", "u"]
+  // In a new array fille with blank and then map to a random value
   const rand = new Array(3).fill('').map(() => {
     return choices[Math.floor(Math.random() * choices.length)]
   })
   return `J${rand[0]}l${rand[1]}d${rand[2]}`
 }
-const generatedEl = document.querySelector('.generated')
-const makeContent = () => {
-  generatedEl.innerText = makeName()
+
+// Define the choices 
+const helloChoices = ["Hi", "你好"]
+let counter = 0
+
+const cycleHello = () => {
+  if (counter < helloChoices.length -1) {
+    counter++
+  } else {
+    counter = 0
+  }
+  return counter
 }
+
+const generatedEl = document.querySelector('.generated')
+
+const makeContent = () => {
+  generatedEl.innerText = helloChoices[cycleHello()]+", I'm Jilada"
+}
+
 makeContent()
-setInterval(makeContent, 5000)
+setInterval(makeContent, 2000)
 
 class Jilada {
   constructor(props) {
